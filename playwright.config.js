b2c41c6f665437d8043ec985863d17d4bd60f672
Akash Ghorpade <akashghorpade93@gmail.com>
2025-06-19 07:27:29 +0530

    Initial Commit

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  
+  
+  use: {
+   
+  
+  },
+
+
+
+});
+
