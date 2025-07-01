00c9a64f47027d4d14c493afe0bfdedeea396525
Akash Ghorpade <akashghorpade93@gmail.com>
2025-07-01 07:28:43 +0530

    E2E product Purchase

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -33,6 +33,45 @@ test.only('@Web Client App login', async ({ page }) => {
     }
     // await page.pause()
 
+    await page.locator("[routerlink='/dashboard/cart']").click();
+    await page.locator("div li").first().waitFor();
+    const isProductAddedToCart = await page.locator(`h3:has-text('${productName}')`).isVisible();
+    expect (isProductAddedToCart).toBeTruthy();
     
-  
+    await page.locator("button:has-text('Checkout')").click();
+    await page.locator(".details__user [type='text']").first().waitFor();
+
+    await page.locator("[placeholder='Select Country']").pressSequentially("india");
+    const dropDown = page.locator(".ta-results").first();
+    await dropDown.waitFor();
+
+    const optionCount = dropDown.locator("button").count();
+    let text;
+    for(let i=0; i < optionCount; i++){
+      text = await dropDown.locator("button").textContent();
+      if(text === " India"){
+        await dropDown.locator("button").nth(i).click();
+      }
+    } 
+    expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+
+    expect(await page.locator(".hero-primary").textContent()).toHaveText(' Thankyou for the order. ');
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderId);
+ 
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+ 
+ 
+    for (let i = 0; i < await rows.count(); ++i) {
+       const rowOrderId = await rows.nth(i).locator("th").textContent();
+       if (orderId.includes(rowOrderId)) {
+          await rows.nth(i).locator("button").first().click();
+          break;
+       }
+    }
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
  })
